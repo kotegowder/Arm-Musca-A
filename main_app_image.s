3963bac8a6c1470112a6c0669d38111ae9069696
Vinay Kumar Kotegowder <VinayKumar.Kotegowder@arm.com>
2018-09-22 20:05:09 +0530

    Adding assembly support files to help load TBSA application

---
+++ main_app_image.s
@@ -0,0 +1,47 @@
+;/*---------------------------------------------------------------------------------------------------------*/
+;/*                                                                                                         */
+;/* Copyright(c) 2013 Nuvoton Technology Corp. All rights reserved.                                         */
+;/*                                                                                                         */
+;/*---------------------------------------------------------------------------------------------------------*/
+
+
+    AREA _image, DATA, READONLY
+
+    EXPORT  LoaderImage_S_Text_Base
+    EXPORT  LoaderImage_S_Text_Limit
+        
+    EXPORT  LoaderImage_S_Data_Base
+    EXPORT  LoaderImage_S_Data_Limit  
+ 
+    EXPORT  LoaderImage_NSC_Entry_Base
+    EXPORT  LoaderImage_NSC_Entry_Limit  
+        
+    EXPORT  LoaderImage_NS_Text_Base
+    EXPORT  LoaderImage_NS_Text_Limit
+        
+    EXPORT  LoaderImage_NS_Data_Base
+    EXPORT  LoaderImage_NS_Data_Limit          
+    
+    ALIGN   4
+        
+LoaderImage_S_Text_Base
+    INCBIN TBSA_Objects\.text    
+LoaderImage_S_Text_Limit
+
+LoaderImage_S_Data_Base
+    INCBIN TBSA_Objects\.data
+LoaderImage_S_Data_Limit
+
+LoaderImage_NSC_Entry_Base
+    INCBIN TBSA_Objects\.tbsa_nsc_entry_points
+LoaderImage_NSC_Entry_Limit
+
+LoaderImage_NS_Text_Base
+    INCBIN TBSA_Objects\.tbsa_ns_text
+LoaderImage_NS_Text_Limit
+
+LoaderImage_NS_Data_Base
+    INCBIN TBSA_Objects\.tbsa_ns_data
+LoaderImage_NS_Data_Limit
+
+	END
---
+++ test_code_image.s
@@ -0,0 +1,19 @@
+;/*---------------------------------------------------------------------------------------------------------*/
+;/*                                                                                                         */
+;/* Copyright(c) 2013 Nuvoton Technology Corp. All rights reserved.                                         */
+;/*                                                                                                         */
+;/*---------------------------------------------------------------------------------------------------------*/
+
+
+    AREA _image, DATA, READONLY
+
+    EXPORT  LoaderImage_Test_Base
+    EXPORT  LoaderImage_Test_Limit
+    
+    ALIGN   4
+        
+LoaderImage_Test_Base
+   INCBIN ./TBSA_Objects/tbsa_test_combined.bin  
+LoaderImage_Test_Limit
+
+    END
